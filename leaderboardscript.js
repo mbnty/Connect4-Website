5d95e31942b0aad2ba4f05cff7b0f02d40625600
RichardPhist <Carloizawesome98@gmail.com>
2022-12-05 18:07:54 -0800

    post not posting

--- leaderboardscript.js
+++ leaderboardscript.js
@@ -1,6 +1,8 @@
 var rowCount = 0;
 var playerInfo;
+var sorted_leaderboard;
 var httpRequest;
+var value;
 var leaderboard_space = document.getElementById("information");
 
 
@@ -40,18 +42,33 @@ function getFromDB() {
     }
 }
 
-function sort_single_column() {
-    console.log("in putIntoPage");
+function sort_single_column(value) {
+    console.log("in sort_single_column");
     httpRequest = new XMLHttpRequest(); // create the object
     if (!httpRequest) { // check if the object was properly created
       // issues with the browser, example: old browser
       alert('Cannot create an XMLHTTP instance');
       return false;
     }
+    let choice = "";
+    console.log("value is: "+value);
+
+    if(value > 0){
+        choice = "DESC";
+    }
+    else{
+        choice = "ASC";
+    }
+
+    console.log("Choice is: "+choice);
+    fd = new FormData();
+    fd.append("order_by", choice);
+    fd.append("order", choice)
+
     httpRequest.onreadystatechange = getFromDB_single; // we assign a function to the property onreadystatechange (callback function)
     httpRequest.open('POST','order_by.php');
     httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    httpRequest.send(); // GET = send with no parameter !
+    httpRequest.send(fd);
 }
   
 function getFromDB_single() {
@@ -60,12 +77,11 @@ function getFromDB_single() {
         if (httpRequest.status === 200) {
           console.log("server status: "+httpRequest.status);
           console.log("server response: "+httpRequest.responseText);
-              playerInfo = JSON.parse(httpRequest.responseText);
-              rowCount = playerInfo.length;
+              sorted_leaderboard = JSON.parse(httpRequest.responseText);
+              rowCount = sorted_leaderboard.length;
               console.log("length of player array: "+rowCount);
-              console.log("PLAYER NAME IS: "+playerInfo[0].Name);
-
-              makeTable();
+              console.log("PLAYER NAME IS: "+sorted_leaderboard[0].Name);
+              updateTable();
         } else {
           alert('There was a problem with the request.');
         }
@@ -74,6 +90,25 @@ function getFromDB_single() {
     catch( e ) { // Always deal with what can happen badly, client-server applications --> there is always something that can go wrong on one end of the connection
       alert('Caught Exception: ' + e.description);
     }
+}
+
+function updateTable(){
+    console.log("I'M IN MAKE TABLE");
+    var table = document.getElementById("leaderboard");
+
+    table.innerHTML = "";
+    var row;
+    var cell;
+    for(let item of sorted_leaderboard){
+        row = document.createElement("tr");
+        for(let field in item){
+            cell = document.createElement("td");
+            cell.innerText = item[field];
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    leaderboard_space.appendChild(table);
 }
 
 function makeTable(){
@@ -98,21 +133,24 @@ function makeTable(){
 
 function flip(header_column){
     //default sort on desc
-    let flipflop = -1;
-    switch(header_column){ //do a specific post on a specific column click
-        case (header_column > 0 || header_column < 0):
-            break;
-        case 2:
-            flipflop * -1;
-            break;
-        case 3:
-            flipflop * -1;
-            break;
-        case 4:
-            flipflop * -1;
-            break;
-    }
-    header_column * flipflop; //switch between asec or desec
+    console.log("INSIDE FLIP: " + header_column);
+    let flipflop = header_column * -1; //switch between desc or asc
+        if(flipflop == -1 || flipflop == 1){
+            console.log(flipflop);
+            sort_single_column(flipflop);
+        }
+        if(flipflop == -2 || flipflop == 2){
+            console.log(flipflop);
+            sort_single_column(flipflop);
+        }
+        if(flipflop == -2 || flipflop == 2){
+            console.log(flipflop);
+            sort_single_column(flipflop);
+        }
+        if(flipflop == -2 || flipflop == 2){
+            console.log(flipflop);
+            sort_single_column(flipflop);
+        }
 }
 
 
--- order_by.php
+++ order_by.php
@@ -1,47 +1,22 @@
 <?php
 require_once "config_gameinfo.php";
 
-/*
-
-USE A POST TO CHECK FOR DECENDING AND ASCENDING
-ORDER OF THE INFORMATION
-A.K.A delete all the functions lol...
-
-*/
-
 $conn = new mysqli($servername, $username, $password, $dbname_game);
 
-function order_total_games(){
-    $sql = "SELECT * FROM leaderboard ORDER BY `total_games` DESC";
-    $result = $conn->query($sql);
-    echo $result;
-    json_encode($result);
-    echo $result;
-}
-
-function order_wins(){
-    $sql = "SELECT * FROM leaderboard ORDER BY `wins` DESC";
-    $result = $conn->query($sql);
-    json_encode($result);
-    echo $result;
-}
-
-function order_time_played(){
-    $sql = "SELECT * FROM leaderboard ORDER BY `time_played` DESC";
+if($_SERVER["REQUEST_METHOD"] == 'POST'){
+    if(isset($_POST["order_by"])){
+        $sql = "SELECT * FROM leaderboard ORDER BY " . $_POST["order_by"];
+    }
+    else if(isset($_POST["order"])){
+        $sql = "SELECT * FROM leaderboard ORDER BY " . $_POST["order"];
+    }
+    else{
+        echo "NO POST FOUND";
+    }
+    
     $result = $conn->query($sql);
     json_encode($result);
     echo $result;
 }
 
-function order_turn_count(){
-    $sql = "SELECT * FROM leaderboard ORDER BY `turn_count` DESC";
-    $result = $conn->query($sql);
-    json_encode($result);
-    echo $result;
-}
-
-order_total_games();
-order_wins();
-order_time_played();
-order_turn_count();
 ?>
