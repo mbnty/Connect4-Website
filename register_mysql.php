02a76a433045a31dca71266fce6db986114073c1
RichardPhist <tyson980@mail.fresnostate.edu>
2022-12-11 20:18:59 -0800

    login and register styling

--- register_mysql.php
+++ register_mysql.php
@@ -119,32 +119,33 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
+    <link rel="stylesheet" href="registerstyle.css">
 </head>
 <body>
-    <div>
+    <div class="flex-container">
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <div class="input-boxes" <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username:<sup>*</sup></label>
                 <input type="text" name="username" value="<?php echo $username; ?>">
-                <span><?php echo $username_err; ?></span>
+                <span class="reg-err"><?php echo $username_err; ?></span>
             </div>    
-            <div <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="input-boxes" <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                 <label>Password:<sup>*</sup></label>
                 <input type="password" name="password" value="<?php echo $password; ?>">
-                <span><?php echo $password_err; ?></span>
+                <span class="reg-err"><?php echo $password_err; ?></span>
             </div>
-            <div <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <div class="input-boxes" <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                 <label>Confirm Password:<sup>*</sup></label>
                 <input type="password" name="confirm_password" value="<?php echo $confirm_password; ?>">
-                <span><?php echo $confirm_password_err; ?></span>
+                <span class="reg-err"><?php echo $confirm_password_err; ?></span>
             </div>
-            <div>
+            <div id="sub-button">
                 <input type="submit" value="Submit">
                 <input type="reset"  value="Reset">
             </div>
-            <p>Already have an account? <a href="login_mysql.php">Login here</a>.</p>
+            <p>Already have an account? <a href="login.html">Login here</a>.</p>
         </form>
     </div>    
 </body>
